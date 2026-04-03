84a32f2784379e3c874eb062506dd1a4895d8929
muhammadyusufyakubjanov8-stack <muhammadyusuf8@gmail.con>
2026-04-03 10:38:07 +0500

    git buyuruqlari darsi

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,1 +1,26 @@
-console.log("Salom")
+console.log("GitHub")
+
+// git init
+
+// git romte add orgini https://repozitory-manzili
+
+// git add .
+
+// git commit -m "comentariya yoziladi"
+
+// git push origin master
+
+// =======================================
+
+// Qushimcha so'rabqolsa:
+
+// git config --global user .email "muhammadyusufyakubjanov8-stack"
+// git config --global user.name "muhammadyusufyakubjanov8-stack"
+
+// =====================================
+
+// Doim yangi code yozsak 3 ta ta git buyurig'ini doim ishlatamiz shrt.
+
+// git add .
+// git commit -m "komentariya yozing"
+// git push origin master
