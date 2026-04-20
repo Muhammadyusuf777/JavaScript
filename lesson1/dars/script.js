42b6154aea1cf979e80a04fcf185929770d13a67
muhammadyusufyakubjanov8-stack <muhammadyusuf8@gmail.con>
2026-04-20 18:18:43 +0500

    prompt confirm alert

--- lesson1/dars/script.js
+++ lesson1/dars/script.js
@@ -1,37 +1,29 @@
-    //   console.log("Muhammad yusuf")
-    //     console.log("2026")
-    //     console.log(20 + 5)
-    //     console.log(20 - 5)
-    //     console.log(20 * 5)
-    //     console.log(20 / 5)
-
-    //     // =======================
-    //     // Ozgaruvchi yaratish 
-    //     var raqam1 = 202620262026
-    //     let matn = "bhfgshfskdskfsd"
-    //     const tugilganYil = 2020
-    //     console .log (raqam1)
-    //     console .log (matn)
-    //     console .log (tugilganYil)
-
-
-    // ########################
-
-    // // alert () - client message(xabar) chiqarish uchun ishlatiladi
-    // alert("Web site mizga xush kelibsiz!")
-
-    // // prompt() - clientga savol berih va qiymat kiritishni uchun ishlatiladi.
-    // let ism = prompt("Ismingizni kiriting")
-    // console .log(ism)
-
-    // // confirm() - Clientga qisqa savol berish uchun ishlatiladi
-    // let savol = confirm("Bugun darsga keldingizmi")
-    // console .log(savol)
-
-    // ##########################
-
-    let hozirgiYil = prompt("Hozirgi yilni kiriting")
-    let tugulganYil = prompt("tugilgan yilingizni kiriting")
-    let yosh = hozirgiYil - tugulganYil
-    alert(`siz ${yosh} yoshdasiz`)
+//   console.log("Muhammad yusuf")
+//     console.log("2026")
+//     console.log(20 + 5)
+//     console.log(20 - 5)
+//     console.log(20 * 5)
+//     console.log(20 / 5)
+//     // =======================
+//     // Ozgaruvchi yaratish 
+//     var raqam1 = 202620262026
+//     let matn = "bhfgshfskdskfsd"
+//     const tugilganYil = 2020
+//     console .log (raqam1)
+//     console .log (matn)
+//     console .log (tugilganYil)
+// ########################
+// // alert () - client message(xabar) chiqarish uchun ishlatiladi
+// alert("Web site mizga xush kelibsiz!")
+// // prompt() - clientga savol berih va qiymat kiritishni uchun ishlatiladi.
+// let ism = prompt("Ismingizni kiriting")
+// console .log(ism)
+// // confirm() - Clientga qisqa savol berish uchun ishlatiladi
+// let savol = confirm("Bugun darsga keldingizmi")
+// console .log(savol)
+// ##########################
+// let hozirgiYil = prompt("Hozirgi yilni kiriting")
+// let tugulganYil = prompt("tugilgan yilingizni kiriting")
+// let yosh = hozirgiYil - tugulganYil
+// alert(`siz ${yosh} yoshdasiz`)
     
