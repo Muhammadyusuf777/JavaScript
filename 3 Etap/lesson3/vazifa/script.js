d841013ec48123daeb04701750d401800b9ffa67
Muhammadyusuf777 <muhammadyusufyakubjanov8@gmail.com>
2026-08-07 09:03:22 +0500

    filter va map

---
+++ 3 Etap/lesson3/vazifa/script.js
@@ -0,0 +1,3 @@
+// sort - array ichidagi qiymatylarini tartiblaydi
+let arr = [4,2,5,8,9,8,9,8,3,]
+let arrSorted = arrSorted.sort()
---
+++ 3 Etap/lesson4/script.js
@@ -0,0 +1,18 @@
+// Masala:Massiv obyektlardan yoshi 18 dan katta va ismida "a" harfi bor bo'lganlarni toping
+// javobi: [{ name: "Ali", age: 25 }, { name: "Sami", age: 20 }]
+
+// let user = [{ name: "Ali", age: 25 }, { name: "Vali", age: 17 }, { name: "Sami", age: 20 }, { name: "John", age: 30 }];
+// let result = user.filter(u => u.age > 18 && u.name.toLowerCase().includes('a'));
+// console.log(result); 
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 10 === 0) {
+    console.log(i);
+  }
+}
